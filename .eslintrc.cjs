66f1eb1eea6a6d20425e4cdefdb132a960ea347c
Lucas Broda <105855047+LucasBroda@users.noreply.github.com>
2026-04-13 07:21:25 +0100

    Build/config du projet (#1)
    * build: Ajout de plugins Vite au projet
    
    * build: Ajout du plugins ESLint
    
    * build: Ajout du plugins Prettier
    
    * build: Installation de React Router
    
    * build: Ajout de la dépendance @types/node pour les définitions de types pour node
    
    * feat(package.json): Ajout d'un script start
    
    * build(vite.config.ts): Ajout d'une config vite
    
    * build(tsconfig.json): Ajout d'éléments de config ts
    
    * build(.eslintrc.cjs): Ajout de configuration pour eslint
    
    * build(.prettierrc): Ajout de config prettier pour tous les fichiers
    
    * feat(main.tsx): Test de la config prettier
    
    * build: Création du répertoire backend
    
    * build: Ajout de dépendances pour la partie back
    
    * build: Ajout de dépendances Typescript pour la partie back
    
    * build: Ajout des dépendances eslint et prettier
    
    * build(tsconfig.json): Config ts
    
    * build(.eslintrc.cjs): Ajout de config eslint
    
    * build(.prettierrc): Ajout de config prettier
    
    * build(package.json): Ajout de scripts
    
    * build(nodemon.json): Ajout de la config nodemon
    
    * feat: v1 de l'app
    
    * feat création d'un CLAUDE.md afin de spécifier des règles pour les prompts
    
    * feat: ajout de règles de style précise + règles d'accessibilité pour le projet
    
    * feat: création d'un fichier Inputs_for_project spécifiant des règles précises pour le mode agent

---
+++ .eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended'
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  plugins: ['react-refresh', '@typescript-eslint', 'prettier'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true }
+    ],
+    'prettier/prettier': 'error',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'warn'
+  }
+}
